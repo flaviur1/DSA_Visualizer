e3460cf3255e877a3ab1f2b913be9d532f9d983a
Romitan Flaviu <flaviu.romitan23@gmail.com>
2024-11-28 17:24:52 +0200

    made an insert button and functions to add elements to the data structure

--- frontend/src/components/UserOperationsBox.jsx
+++ frontend/src/components/UserOperationsBox.jsx
@@ -2,6 +2,7 @@
 // We also have the start button, which sends a request to the backend to get the json steps so we can print them.
 import { useState, useEffect } from "react";
 import axios from "axios";
+import TextField from "@mui/material/TextField";
 
 function UserOperationsBox({
   dataStructure,
@@ -10,7 +11,6 @@ function UserOperationsBox({
   onStepsReceived,
 }) {
   const [newElement, setNewElement] = useState("");
-  const addElem = (arr, elem) => {};
 
   const handleSubmit = async () => {
     try {
@@ -26,9 +26,32 @@ function UserOperationsBox({
     }
   };
 
+  const handleInsert = async () => {
+    if (newElement === "") {
+      alert("Please insert the element first");
+      return;
+    }
+    if(parseInt(newElement) === null){
+      alert("Please insert an integer only");
+      return;
+    }
+    setDataStructure([...dataStructure, parseInt(newElement)]);
+    setNewElement("");
+  };
+
   return (
     <div>
       <button onClick={handleSubmit}>Start Algorithm</button>
+      <TextField
+        id="standard-basic"
+        label="Insert"
+        variant="standard"
+        value={newElement}
+        onChange={(e) => {
+          setNewElement(e.target.value);
+        }}
+      />
+      <button onClick={handleInsert}>Submit</button>
       <p>Current Array: {JSON.stringify(dataStructure)}</p>
     </div>
   );
